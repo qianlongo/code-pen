955bcf3a975da5aa37601c8407919d731ec7cd6b
qianlongo <1162633511@qq.com>
2019-04-02 21:37:31 +0800

    update:

--- fetch/demo.js
+++ fetch/demo.js
@@ -1,3 +1,18 @@
+fetch('http://3157-axsqb-sl-creditcard.fin.test.sankuai.com/api/apply/apply/api/getSafetyTips?cardMetaId=4&crossMarketingTarget=0')
+      .then((res) => {
+        return res.json()
+      }).then((res) => {
+        const { bankLogoPicUrl } = res.data
+        const img = document.createElement('img')
+
+        img.src = bankLogoPicUrl
+        document.body.appendChild(img)
+        console.log('-----fecth')
+      }).catch(() => {
+        console.log('fetch error')
+      })
+
+
 var i = 0
 
 while (i < 30) {
