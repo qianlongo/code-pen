3cd1aa7546d743203923e588098a9fb45ca6fcba
qianlongo <1162633511@qq.com>
2019-04-04 10:23:43 +0800

    update: compose实现

---
+++ base/test.js
@@ -0,0 +1,39 @@
+const compose = (cbs) => {
+  let next = () => {}
+  let len = cbs.length
+  
+  while (--len) {
+    next = cbs[ len ].bind(null, next)
+  }
+
+  for (let i = len - 1; i >= 0; i--) {
+    next = cbs[ i ].bind(null, next)
+  }
+
+  return next
+}
+
+const timeout1 = (next) => {
+  setTimeout(() => {
+    console.log(1)
+    next()
+  }, 300)
+}
+
+const timeout2 = (next) => {
+  setTimeout(() => {
+    console.log(2)
+    next()
+  }, 200)
+}
+
+const timeout3 = (next) => {
+  setTimeout(() => {
+    console.log(3)
+    next()
+  }, 100)
+}
+
+// timeout1.bind(null, timeout2.bind(null, timeout3.bind(null, () => {})))()
+
+compose([ timeout1, timeout2, timeout3 ])()
